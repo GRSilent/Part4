96e5e6fdd2414426b882c0e981e9d165d437ef5d
Krybe <Krybe@SILENT>
2024-05-19 14:52:23 +0200

    Добавьте файлы проекта.

---
+++ Part4/4.1.cs
@@ -0,0 +1,37 @@
+﻿namespace Part4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество строк: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите колличество столбцов: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[row, col];
+
+            Random r = new Random();
+
+            for (int i = 0; i < row; i++) 
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr[i, j] = r.Next(10);
+                    Console.Write($"{arr[i, j]} ");
+                    
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+
+            for (int s = 0; s < row; s++)
+                for (int k = 0; k < col; k++)
+                    sum += arr[s, k];
+
+            Console.WriteLine("Сумма всех элементов двухмерного массива равна: {0}", sum);
+
+        }
+    }
+}
